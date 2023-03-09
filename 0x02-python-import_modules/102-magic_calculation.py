c5b47bbdcb928f34caf92436aadf70911f017879
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-03-09 04:58:54 -0800

    Magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
+    """Match bytecode provided"""
     from magic_calculation_102 import add, sub
+
     if a < b:
-        addition = add(a, b)
-        for i in range(4, 6):
-            addition = add(addition, i)
-        return addition
+        return sum([add(a, b) for _ in range(2)]) + add(4, 5)
     else:
         return (sub(a, b))
 
