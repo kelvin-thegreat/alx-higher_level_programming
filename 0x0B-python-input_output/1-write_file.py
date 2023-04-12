915cda6c61f6a441ac8898e6ec8e1fee598e9ee6
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-04-12 15:07:54 -0700

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        count = file.write(text)
+    return count    
