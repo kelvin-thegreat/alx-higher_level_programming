5114a2421e9f595901a44fc26a90335cc52f28c9
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-04-16 11:13:14 -0700

    Advance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ This class will be the “base” of all other classes in this project"""
 
-
 import json
+import turtle
+import tkinter as TK
+
 
 
 class Base:
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Class constructor"""
+        """class Constructor"""
         if id is not None:
             self.id = id
         else:
@@ -26,12 +29,12 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
-        filename = "{}.json".format(cls.__name__)
         if list_objs is None:
             list_objs = []
-        list_dicts = [obj.to_dictionary() for obj in list_objs]
-        with open(filename, mode='w', encoding='utf-8') as file:
-            file.write(cls.to_json_string(list_dicts))
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as file:
+            objs = [obj.to_dictionary() for obj in list_objs]
+            file.write(cls.to_json_string(objs))
 
     @staticmethod
     def from_json_string(json_string):
@@ -53,12 +56,42 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """Returns a list of instances"""
-        filename = "{}.json".format(cls.__name__)
+        filename = cls.__name__ + ".json"
         try:
-            with open(filename, mode='r', encoding='utf-8') as file:
-                json_string = file.read()
-                list_dicts = cls.from_json_string(json_string)
-                return [cls.create(**d) for d in list_dicts]
+            with open(filename, mode="r", encoding="utf-8") as file:
+                objs = cls.from_json_string(file.read())
+                return [cls.create(**obj) for obj in objs]
         except FileNotFoundError:
             return []
 
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes list_objs to CSV file"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", encoding="utf-8") as file:
+            if cls.__name__ == "Rectangle":
+                for obj in list_objs:
+                    file.write("{},{},{},{},{}\n".format(obj.id, obj.width, obj.height, obj.x, obj.y))
+            elif cls.__name__ == "Square":
+                for obj in list_objs:
+                    file.write("{},{},{},{}\n".format(obj.id, obj.size, obj.x, obj.y))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes CSV file to list of objects"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, mode="r", encoding="utf-8") as file:
+                objs = []
+                for line in file:
+                    fields = line.strip().split(",")
+                    if cls.__name__ == "Rectangle":
+                        obj = cls(int(fields[0]), int(fields[1]), int(fields[2]), int(fields[3]), int(fields[4]))
+                    elif cls.__name__ == "Square":
+                        obj = cls(int(fields[0]), int(fields[1]), int(fields[2]), int(fields[3]))
+                    objs.append(obj)
+                return objs
+        except FileNotFoundError:
+            return []
