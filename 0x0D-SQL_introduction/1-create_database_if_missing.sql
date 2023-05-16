93def44abb287faadca4725c79585255fd425ab8
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-05-16 03:19:23 -0700

    Create DB

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Script: Create hbtn_0c_0 database
+-- Description: This script creates the hbtn_0c_0 database if it doesn't already exist.
+
+-- Create the hbtn_0c_0 database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
