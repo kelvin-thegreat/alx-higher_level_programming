99d8c7d16c9dbceffc548b9821f5904a080c9d6d
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-05-16 03:51:25 -0700

    Show Tables

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- Script: List all tables of a database
+-- Description: This script lists all the tables in a specified database.
+-- Set the desired database name
+USE mysql;
+-- Retrieve the list of tables in the database
+SHOW TABLES;
