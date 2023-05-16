b1dbea163bdf01105acb97ef5ea4dd0626d885c8
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-05-16 03:24:25 -0700

    Delete DB

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script: Delete hbtn_0c_0 database
+-- Description: This script deletes the hbtn_0c_0 database if it exists.
+
+-- Drop the hbtn_0c_0 database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
