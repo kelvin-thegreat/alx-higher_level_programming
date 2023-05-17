64db28bb192635359d1fe2490c0b5ada64e92a0a
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-05-17 04:17:28 -0700

    adv3

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,8 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
 -- Results must be sorted in descending order by their rating
-SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
-FROM hbtn_0d_tvshows_rate
-JOIN tv_shows ON hbtn_0d_tvshows_rate.show_id = tv_shows.id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+SELECT `title`, SUM(`rate`) AS `rating`
+FROM `tv_shows` AS tv
+INNER JOIN `tv_show_ratings` AS ra
+ON tv.`id` = ra.`show_id`
+GROUP BY `title`
+ORDER BY `rating` DESC;
