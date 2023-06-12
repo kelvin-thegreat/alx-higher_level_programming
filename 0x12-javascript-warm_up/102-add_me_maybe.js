0dcbe84eec1857fe4494ea6f75c38d954804d417
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-12 08:40:58 -0700

    Add me Maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -2,8 +2,6 @@
 
 // Write a function that increments and calls a function.
 // The function must be visible from outside
-// Prototype: function (number, theFunction)
-// No using var keyword
 
 exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number);
