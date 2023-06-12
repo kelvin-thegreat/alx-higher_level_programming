d27cdcc45874752c7134da5f276e37836ea50fbd
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-12 06:31:42 -0700

    Value of Args

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+
 // script that prints the first argument passed to it
 // If no arguments are passed to the script, print “No argument”
+
 console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
 
