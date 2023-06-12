9de3b76388dd918f35a4fccea0d166fc9f43ffff
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-12 08:17:27 -0700

    Add File

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// function that returns the addition of 2 integers.
+// unction must be visible from outside
+// The name of the function must be add
+
+exports.add = (num1, num2) => num1 + num2;
