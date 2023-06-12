ad0b74d3ee8490b679a17649e732ad26381ca034
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-12 08:33:24 -0700

    Call me Moby

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// ifunction that executes x times a function.
+// The function must be visible from outside
+// Prototype: function (x, theFunction)
+
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
+};
