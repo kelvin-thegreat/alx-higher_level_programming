6844659f3ab836e4a60692848f27a3197942e42e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-13 07:21:58 -0700

    Factor Index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,8 +3,5 @@
 // script that imports an array and computes a new array
 
 const list = require('./100-data').list;
-
-const newList = list.map((value, index) => value * index);
-
-console.log('Initial list:', list);
-console.log('New list:', newList);
+console.log(list);
+console.log(list.map((value, index) => value * index));
