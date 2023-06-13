b9d5f2a21a26d56f09803c97dd8663a299081d9c
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-13 07:36:43 -0700

    Concat Files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-// script that concats 2 files 
-
 const fs = require('fs');
 const file1 = fs.readFileSync(process.argv[2], 'utf8');
 const file2 = fs.readFileSync(process.argv[3], 'utf8');
