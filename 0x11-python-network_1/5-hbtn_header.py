b73365507ca626ea962e51d404703174561bbb26
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-30 02:44:49 -0700

    Header Value Response

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -14,6 +14,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-
-    print("X-Request-Id:", x_request_id)
+    print(response.headers.get('X-Request-Id'))
