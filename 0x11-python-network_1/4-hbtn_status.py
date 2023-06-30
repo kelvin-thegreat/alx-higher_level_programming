a4c893c476f95d5502f2d461435ad36f9897c4c9
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-30 02:35:23 -0700

    what's my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import requests
+
+if __name__ == "__main__":
+    url = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
+
