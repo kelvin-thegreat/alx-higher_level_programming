3a4ee46ddb47e190443a005a5c6a3dcb9941978f
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-07-25 14:47:49 -0700

    js-web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('An error occurred while fetching the URL:', error);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', function (err) {
+      if (err) {
+        console.error('An error occurred while writing to the file:', err);
+      }
+    });
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    console.error('Failed to fetch the URL. Status code:', response.statusCode);
   }
 });
+
