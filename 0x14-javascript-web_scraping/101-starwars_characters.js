5eb244861603bb28c53d716b01ff39619e93f3f4
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-07-25 15:05:51 -0700

    Advanced js-web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,38 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('An error occurred while fetching the API:', error);
+  } else if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    const printCharacters = (characters, index) => {
+      if (index >= characters.length) {
+        return;
+      }
+
+      request(characters[index], function (charError, charResponse, charBody) {
+        if (charError) {
+          console.error('An error occurred while fetching character data:', charError);
+        } else if (charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+          printCharacters(characters, index + 1);
+        } else {
+          console.error('Failed to fetch character data. Status code:', charResponse.statusCode);
+        }
+      });
+    };
+
     printCharacters(characters, 0);
+  } else {
+    console.error('Failed to fetch the API. Status code:', response.statusCode);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
