391d6d7a370d22777d55ea8edacf1a9ee3740c0e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-07-25 14:40:27 -0700

    js-web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const apiURL = process.argv[2];
 const characterId = 18;
 
@@ -10,7 +9,7 @@ request(apiURL, function (error, response, body) {
     console.error(error);
   } else if (response.statusCode === 200) {
     const data = JSON.parse(body);
-    const moviesWithWedge = data.results.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+    const moviesWithWedge = data.results.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
     console.log(`${moviesWithWedge.length}`);
   } else {
     console.log(`An error occurred. Status code: ${response.statusCode}`);
