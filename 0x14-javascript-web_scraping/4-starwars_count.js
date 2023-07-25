406e13169380025dfa5c2f14f3570b0a7e1521eb
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-07-25 14:36:08 -0700

    js-web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,42 +1,19 @@
 #!/usr/bin/node
 
-const axios = require('axios');
-const url = process.argv[2];
+const request = require('request');
 
-const getNumberOfMoviesWithCharacter = async (apiUrl, characterId) => {
-  try {
-    const response = await axios.get(apiUrl);
-    if (response.status === 200) {
-      const films = response.data.results;
-      return films.reduce((count, film) => {
-        if (film.characters.some((character) => character.includes(`/${characterId}/`))) {
-          count++;
-        }
-        return count;
-      }, 0);
-    } else {
-      console.log('An error occurred. Status code:', response.status);
-      return null;
-    }
-  } catch (error) {
-    console.log('An error occurred:', error.message);
-    return null;
+const apiURL = process.argv[2];
+const characterId = 18;
+
+request(apiURL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const moviesWithWedge = data.results.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+    console.log(`${moviesWithWedge.length}`);
+  } else {
+    console.log(`An error occurred. Status code: ${response.statusCode}`);
   }
-};
-
-if (!url) {
-  console.log('Usage: node movie_counter.js <API_URL>');
-} else {
-  const characterId = 18;
-
-  getNumberOfMoviesWithCharacter(url, characterId)
-    .then((count) => {
-      if (count !== null) {
-        console.log(`The number of movies where the character 'Wedge Antilles' is present: ${count}`);
-      }
-    })
-    .catch((error) => {
-      console.log('An error occurred:', error.message);
-    });
-}
+});
 
