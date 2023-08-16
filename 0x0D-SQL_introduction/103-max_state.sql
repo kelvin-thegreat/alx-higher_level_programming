5d6fb777ae36d3b6a976ae8fd36dad50a27ab2c6
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-16 01:44:00 -0700

    Updated Temperatures #2

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,5 @@
 -- script that displays the max temperature of each state
-SELECT state, MAX(temperature) AS max_temperature
-FROM temperature_table
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
 ORDER BY state;
