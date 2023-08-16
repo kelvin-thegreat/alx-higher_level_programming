92bd835903f41a7c8cf700db82916f864d916396
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-16 02:17:15 -0700

    Correction on . Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,9 +3,10 @@
 -- Group the results by genre
 -- Filter out genres that don't have any shows linked
 -- Sort the results in descending order by the number of shows linked
-
-SELECT genre AS genre, COUNT(*) AS number_of_shows
-FROM hbtn_0d_tvshows
-GROUP BY genre
-HAVING COUNT(*) > 0
-ORDER BY number_of_shows DESC;
+SELECT gen.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS gen
+       INNER JOIN `tv_show_genres` AS tvs
+       ON gen.`id` = tvs.`genre_id`
+ GROUP BY gen.`name`
+ ORDER BY `number_of_shows` DESC;
