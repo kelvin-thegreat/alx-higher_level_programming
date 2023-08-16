fa4a2ec97507f3f96449a64c4ac49804c6cb2bd0
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-16 01:39:39 -0700

    Correction of 19. Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- script that displays the top 3 of cities temperature
-SELECT city, temperature
-FROM temperature_table
-WHERE month IN (7, 8)
-ORDER BY temperature DESC
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
-
