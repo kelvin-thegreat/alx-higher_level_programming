134c4f6637e1b2965a0b612a458fe1d463ece85f
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-08-16 01:05:59 -0700

    Full Description Correction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- script that prints the full description of the table first_table from the database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
